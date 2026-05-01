84fdf21e6c105ea15ef399050f6a0511218dc5ac
jfurdzik <88293361+jfurdzik@users.noreply.github.com>
2026-05-01 17:24:37 -0400

    evaluation done, debugged, code finished

--- neural_net.py
+++ neural_net.py
@@ -3,25 +3,18 @@ from torch import nn
 import random
 import numpy as np
 
-K = 4 # sample size
-
 class SoilNet(nn.Module):
     def __init__(self):
         super().__init__()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(2*K, 32),
+            nn.Linear(2, 32),
             nn.ReLU(),
             nn.Dropout(0.2),
             nn.Linear(32, 16),
             nn.ReLU(),
-            nn.Linear(16, 1),
-            nn.Sigmoid()
+            nn.Linear(16, 1)
         )
 
     def forward(self, x):
-        x = self.flatten(x)
         logits = self.linear_relu_stack(x)
         return logits
-    
-    # TODO make binary sigmoid wet/dry, flatten input
-    
--- soil_calculations.py
+++ soil_calculations.py
@@ -4,14 +4,16 @@ import numpy as np
 import neural_net
 import random
 from torch.utils.data import random_split, Dataset, ConcatDataset, RandomSampler, DataLoader
-from torch.nn import BCELoss
+from torch.nn import MSELoss
 from torch.optim import SGD
+import torch
+import matplotlib.pyplot as plt
 
 # global constants
 d2 = 0.3 # reflector buried at depth of 30cm = 0.3m
 c = 3e8 # speed of light in a vacuum
 K = 4 # sample size
-EPOCHS = 20 # change later
+EPOCHS = 100 # change later
 
 # get top 2 highest amps and their corresponding ToFs
 def get_max_amps(df):
@@ -50,7 +52,8 @@ def calc_RI(t1, t2):
 
 # RAR or p calculation using formula defined in paper
 def calc_RAR(a1, a2):
-    return a2 / a1
+    eps = 1e-8 # prevent divide by 0
+    return a2 / (a1+eps)
 
 class SoilDataset(Dataset):
     def __init__(self, X, y):
@@ -63,30 +66,30 @@ class SoilDataset(Dataset):
     def __getitem__(self, idx):
         return self.X[idx], self.y[idx]
 
-# sample K data points matching indices n and p and flatten/concat
-def sample_K(dataset):
-    # need to get the same matching n and p elem
-    idx = random.sample(range(len(dataset)))
-    n_vec = [dataset[i][0] for i in idx] # n is first elem in tuple
-    p_vec = [dataset[i][1] for i in idx] # p second elem in tuple
-    x = np.concatenate([n_vec, p_vec])
-    return x
-
 
 def preprocessing_data(n_dry, p_dry, n_wet, p_wet):
     # --------- TRAINING + NN STUFF HERE ----------------
-    # also split data 80/20 each wet/dry for training vs testing
-    X_dry = np.column_stack((n_dry, p_dry))
-    X_wet = np.column_stack((n_wet, p_wet)) # TODO REVERT THIS FLATTEN LATER after split
-    y_dry = np.zeros(X_dry.shape[0])
-    y_wet = np.ones(X_wet.shape[0])
+    
+    X_dry = torch.stack((n_dry, p_dry), dim=1)
+    X_wet = torch.stack((n_wet, p_wet), dim=1) # TODO REVERT THIS FLATTEN LATER after split
+
+    # normalize Xs
+    X_dry = (X_dry - X_dry.mean(dim=0)) / X_dry.std(dim=0)
+    X_wet = (X_wet - X_wet.mean(dim=0)) / X_wet.std(dim=0)
+
+    y_dry = torch.full((len(X_dry),), 0.1)
+    y_wet = torch.full((len(X_wet),), 0.4)
     dataset_dry = SoilDataset(X_dry, y_dry)
     dataset_wet = SoilDataset(X_wet, y_wet)
 
-    train_size = int(0.8*len(X_dry))
-    test_size = len(X_dry) - train_size
-    train_data_dry, test_data_dry = random_split(dataset_dry, [train_size, test_size])
-    train_data_wet, test_data_wet = random_split(dataset_wet, [train_size, test_size]) # is wet same size as dry? bc channels?
+    # split data 80/20 each wet/dry for training vs testing
+    train_size_dry = int(0.8*len(X_dry))
+    test_size_dry = len(X_dry) - train_size_dry
+    train_size_wet = int(0.8*len(X_wet))
+    test_size_wet = len(X_wet) - train_size_wet
+    train_data_dry, test_data_dry = random_split(dataset_dry, [train_size_dry, test_size_dry])
+    train_data_wet, test_data_wet = random_split(dataset_wet, [train_size_wet, test_size_wet]) # is wet same size as dry? bc channels?
+    # recombine wet and dry to single dataset each for train, test
     train_set = ConcatDataset([train_data_dry, train_data_wet])
     test_set = ConcatDataset([test_data_dry, test_data_wet])
     
@@ -95,26 +98,103 @@ def preprocessing_data(n_dry, p_dry, n_wet, p_wet):
 # training loop for neural net
 def train(dataloader):
     net = neural_net.SoilNet()
-    loss_func = BCELoss()
+    loss_func = MSELoss()
     optimizer = SGD(net.parameters(), lr=0.001, momentum=0.9)
+    loss_arr = []
 
     for epoch in range(EPOCHS):
-        print("EPOCH: ", epoch)
+        print("EPOCH: ", epoch+1)
         net.train(True)
         running_loss = 0
         for data in dataloader:
             inputs, labels = data
             optimizer.zero_grad()
-            outputs = net(inputs)
-            loss = loss_func(outputs, labels)
+            outputs = net(inputs.float())
+            loss = loss_func(outputs.squeeze(), labels.float())
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
         print("Running Loss This Epoch: ", running_loss)
+        loss_arr.append(running_loss) # loss for all epochs
     print("TRAINING DONE")
-    
-def evaluate(dataloader):
-    return None # TODO
+    return loss_arr, net #need the smae net for eval
+
+def plot_loss(loss_arr):
+    # plot total loss per epoch 
+    fig, ax = plt.subplots(figsize=(6, 4))
+
+    ax.plot(range(len(loss_arr)), loss_arr)
+    ax.set_xlabel("Epoch")
+    ax.set_ylabel("Total Loss Per Epoch")
+    ax.set_title(f"Total Loss Across All Epochs")
+    ax.grid(True)
+
+    plt.show()
+
+def evaluate_metrics(dataloader, net):
+    net.eval()
+
+    # for mae and rmse
+    abs_errors = []
+    sq_errors = []
+
+    # for predicted vs actual plot
+    preds_all = []
+    y_all = []
+
+    # for dry vs wet historgram
+    dry_preds = []
+    wet_preds = []
+
+    with torch.no_grad():
+        for x, y in dataloader:
+            # pass data thru model
+            preds = net(x.float()).squeeze()
+            y = y.float()
+
+            # mse, rmse
+            abs_errors.append(torch.abs(preds - y))
+            sq_errors.append((preds - y) ** 2)
+            # pred vs actual
+            preds_all.append(preds)
+            y_all.append(y.float())
+
+    # calc mae and rmse
+    abs_errors = torch.tensor(abs_errors)
+    sq_errors = torch.tensor(sq_errors)
+    mae = abs_errors.mean().item()
+    rmse = torch.sqrt(sq_errors.mean()).item()
+
+    preds_all = np.asarray(preds_all)
+    y_all = np.asarray(y_all)
+
+    # plot predicitons vs actual
+    plt.figure()
+    plt.scatter(y_all, preds_all)
+    # perfect predictions line:
+    plt.plot([y_all.min(), y_all.max()], [y_all.min(), y_all.max()], linestyle="--")
+    plt.xlabel("Actual")
+    plt.ylabel("Predicted")
+    plt.title("Predicted vs. Actual Soil Moisture")
+    plt.show()
+
+    # plot historgram dry vs wet preds
+    # histogram dry vs wet
+    for p, label in zip(preds_all, y_all):
+        if 0 <= label.item() <= 0.3:
+            dry_preds.append(p.item())
+        else:
+            wet_preds.append(p.item())
+    plt.figure()
+    plt.hist(dry_preds, bins=20, alpha=0.6, label="Dry")
+    plt.hist(wet_preds, bins=20, alpha=0.6, label="Wet")
+    plt.xlabel("Predicted Moisture")
+    plt.ylabel("Count")
+    plt.title("Prediction Distribution: Dry vs. Wet")
+    plt.legend()
+    plt.show()
+
+    return mae, rmse
 
 def main():
     # read dry soil csvs (for all 10 signals)
@@ -137,19 +217,24 @@ def main():
         n_wet += all_n
         p_wet += all_p
     
-    n_dry = np.asarray(n_dry)
-    p_dry = np.asarray(p_dry)
-    n_wet = np.asarray(n_wet)
-    p_wet = np.asarray(p_wet)
+    n_dry = torch.tensor(n_dry)
+    p_dry = torch.tensor(p_dry)
+    n_wet = torch.tensor(n_wet)
+    p_wet = torch.tensor(p_wet)
 
-    # preprocess/split data, then sample K for net
-    train_set, test_set = preprocessing_data(n_dry, p_dry, n_wet, p_wet) 
-    K_samples = sample_K(train_set)
-    dl = DataLoader(K_samples)
-    train(dl)
+    # preprocess and split up data
+    train_set, test_set = preprocessing_data(n_dry, p_dry, n_wet, p_wet)
 
-    # TODO at some point concat and flatten (X is n and p)??
-    # TODO need to normalize data since n and p dif scales
+    # TRAINING: sample K for net, pass thru with grad descent optim etc
+    dl = DataLoader(train_set, batch_size=K, shuffle=True) 
+    loss, net = train(dl)
+    plot_loss(loss)
+
+    #EVAL: get metrics on test set (mae + rmse, plot residuals, plot histogram wet/dry)
+    mae, rmse = evaluate_metrics(test_set, net) # same net we trained
+    print("MAE: ", mae)
+    print("RMSE: ", rmse)
+
 
 if __name__ == '__main__':
     main()
